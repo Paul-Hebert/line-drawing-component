0f03948c32f3afd8c2cae9615a5707805b217ca9
Paul Hebert <paul@cloudfour.com>
2023-12-02 20:14:34 -0800

    Stubbing out docs

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("**/*.js");
-  eleventyConfig.addPassthroughCopy("**/*.css");
+  eleventyConfig.addPassthroughCopy("line-drawing-component.js");
+  eleventyConfig.addPassthroughCopy("styles.css");
 };
